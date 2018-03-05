fa164803f574efeeed4a82a48bc1d3f7db41727d
Alyssa Watson <alyssa.s.watson@gmail.com>
2018-03-05 00:26:31 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -1,5 +1,13 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+  get '/' do
+    erb :index
+  end
 
+  post '/piglatinize' do
+    pl = PigLatinizer.new
+    @piglatin = pl.to_pig_latin(params[:user_phrase])
+    erb :results
+  end
 end
